254bec33965726c28b30569b02d448409ac1a275
Matteo Karl Donati <matteokarldonati@Matteos-MacBook-Pro.local>
2018-11-27 13:56:18 -0500

    refactoring

--- s_x_h.py
+++ s_x_h.py
@@ -1,49 +1,43 @@
-import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
+from sklearn.linear_model import LogisticRegression
 
-from utils import table
-
-df = table([i for i in range(2000, 2004)])
-
-df_1 = pd.DataFrame(data=None, index=df.index, columns=["pts_diff", "W"])
-i = 0
-
-for index, row in df.iterrows():
-    df_1.iloc[index]["pts_diff"] = row["Pts_diff_home_1"]
-
-    if row["Pts_diff_home_2"] > 0:
-        df_1["W"].iloc[index] = 0
-
-    else:
-        df_1.iloc[index]["W"] = 1
-
-X = df_1["pts_diff"].values.reshape(-1, 1)
-y = df_1["W"].values.reshape(-1, 1)
-
-clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
-a = [i[0] for i in X]
-print(clf.predict_proba(X)[:, 1])
-plt.plot(a, clf.predict_proba(X)[:, 1])
-
-# and plot the result
-plt.figure(1, figsize=(4, 3))
-plt.clf()
-plt.scatter(X.ravel(), y, color='black', zorder=20)
-X_test = np.linspace(-70, 70, 300)
+from utils import home_and_home_data
 
 
 def model(x):
     return 1 / (1 + np.exp(-x))
 
 
-loss = model(X_test * clf.coef_ + clf.intercept_).ravel()
-plt.plot(X_test, loss, color='red', linewidth=3)
+def logistic_regression(X, y):
+    """
+    :param X: array
+    :param y: array
+    :return:
+    """
+    clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
 
-plt.show()
+    print(clf.coef_, clf.intercept_)
 
-print(clf.coef_, clf.intercept_)
+    # PLOT
+
+    plt.scatter(X, y, color='black')
+
+    X_test = np.linspace(-70, 70, 300)
+
+    loss = model(X_test * clf.coef_ + clf.intercept_).ravel()
+    plt.plot(X_test, loss, color='red', linewidth=3)
+
+    plt.show()
 
 
+if __name__ == "__main__":
+    df = home_and_home_data([i for i in range(2000, 2004)])
+
+    X = df["pts_diff_home"].values.reshape(-1, 1)
+    y = df["W"].values.ravel()
+
+    y = y.astype('int')
+
+    logistic_regression(X, y)
--- utils.py
+++ utils.py
@@ -1,43 +1,50 @@
-import pandas as pd
 import sqlite3
+
+import pandas as pd
 
 from config import Config
 
-# Open the connection
-conn = sqlite3.connect(Config.DB_PATH)
-cur = conn.cursor()
+connection = sqlite3.connect(Config.DB_PATH)
 
 
-def table(years, c=cur):
-    '''
+def home_and_home_data(years, conn=connection):
+    """
+    :param years: list
+    :param conn: connection to the database
+    :return: DataFrame columns: ["pts_diff_home", "pts_diff_away", "W"]
+    """
+    c = conn.cursor()
 
-    :param years: list of years you want to take data from
-    :param c: cursor to the database
-    :return: dataframe with columns['Team_1_1', 'Team_2_1', 'Team_1_points_1', 'Team_2_points_1', 'OT_1', 'Team_1_2',
-                                    'Team_2_2', 'Team_1_points_2', 'Team_2_points_2', 'OT_2', 'Season']
-    '''
     result = []
     for year in years:
-        cur.execute(f"SELECT A.Team_1, A.Team_2, A.Team_1_points, A.Team_2_points, A.OT, " \
-                    f"B.Team_1, B.Team_2, B.Team_1_points, B.Team_2_points, B.OT, B.SEASON " \
-                    f"FROM Scores AS A " \
-                    f"INNER JOIN " \
-                    f"Scores AS B " \
-                    f"ON (A.Team_1 = B.Team_2 AND A.Team_2 = B.Team_1) " \
-                    f"WHERE A.Season = '{year}' AND B.Season = '{year}' " \
-                    f"AND A.Neutral = 0 AND B.Neutral = 0 " \
-                    f"AND A.Type = 'REG' AND B.Type = 'REG'")
+        c.execute(f"SELECT A.Team_1, A.Team_2, A.Team_1_points, A.Team_2_points, A.OT, "
+                  f"B.Team_1, B.Team_2, B.Team_1_points, B.Team_2_points, B.OT, B.SEASON "
+                  f"FROM Scores AS A "
+                  f"INNER JOIN "
+                  f"Scores AS B "
+                  f"ON (A.Team_1 = B.Team_2 AND A.Team_2 = B.Team_1) "
+                  f"WHERE A.Season = '{year}' AND B.Season = '{year}' "
+                  f"AND A.Neutral = 0 AND B.Neutral = 0 "
+                  f"AND A.Type = 'REG' AND B.Type = 'REG'")
 
         result.extend(c.fetchall())
 
-    # Write all of them in a dataframe
-    df = pd.DataFrame(data=result,
-                      columns=['Team_1_1', 'Team_2_1', 'Team_1_points_1', 'Team_2_points_1', 'OT_1', 'Team_1_2',
-                               'Team_2_2', 'Team_1_points_2', 'Team_2_points_2', 'OT_2', 'Season'])
+    c.close()
+    conn.close()
 
-    # Add column point differential and set it to zero where the game went to overtime
-    df['Pts_diff_home_1'] = df['Team_1_points_1'] - df['Team_2_points_1']
-    df['Pts_diff_home_1'][df['OT_1'] == True] = 0
-    df['Pts_diff_home_2'] = df['Team_1_points_2'] - df['Team_2_points_2']
+    data = pd.DataFrame(data=result,
+                        columns=['Team_1_H', 'Team_2_A', 'Team_1_H_points', 'Team_2_A_points', 'OT_1', 'Team_1_A',
+                                 'Team_2_H', 'Team_2_H_points', 'Team_1_A_points', 'OT_2', 'Season'])
+
+    df = pd.DataFrame(data=None, index=data.index, columns=["pts_diff_home", "pts_diff_away", "W"])
+
+    df['pts_diff_home'] = data['Team_1_H_points'] - data['Team_2_A_points']
+    df['pts_diff_home'][data['OT_1'] == 1] = 0
+    df['pts_diff_away'] = data['Team_1_A_points'] - data['Team_2_H_points']
+
+    mask = df['pts_diff_away'] > 0
+
+    df["W"][mask] = 1
+    df["W"][~mask] = 0
 
     return df
