3ec1bd3204b1bde97ae8a7210edf2d8bd3fe9db8
ettoredangelo <ettoredangelo97@gmail.com>
2018-11-27 11:39:14 -0500

    documentazione

--- utils.py
+++ utils.py
@@ -7,6 +7,13 @@ cur = conn.cursor()
 
 
 def table(years, c=cur):
+    '''
+
+    :param years: list of years you want to take data from
+    :param c: cursor to the database
+    :return: dataframe with columns['Team_1_1', 'Team_2_1', 'Team_1_points_1', 'Team_2_points_1', 'OT_1', 'Team_1_2',
+                                    'Team_2_2', 'Team_1_points_2', 'Team_2_points_2', 'OT_2', 'Season']
+    '''
     result = []
     for year in years:
         cur.execute(f"SELECT A.Team_1, A.Team_2, A.Team_1_points, A.Team_2_points, A.OT, " \
